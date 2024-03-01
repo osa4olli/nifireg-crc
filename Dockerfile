3a2c99dbb8782dbe5823472086da2d1f00b32947
Olli Vormoor <osa4olli@web.de>
2024-03-01 23:33:46 +0100

    cleaned oidc cfg

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM apache/nifi-registry:1.25.0
 
-USER root
+#USER root
 
-RUN apt-get update
-RUN apt-get install -y vim
+#RUN apt-get update
+#RUN apt-get install -y vim
 
 RUN wget https://jdbc.postgresql.org/download/postgresql-42.7.1.jar -O /opt/nifi-registry/nifi-registry-current/lib/postgresql-42.7.1.jar
 
--- login_oidc_provider.sh
+++
@@ -1,8 +0,0 @@
-scripts_dir='/opt/nifi-registry/scripts'
-
-[ -f "${scripts_dir}/common.sh" ] && . "${scripts_dir}/common.sh"
-
-prop_replace 'nifi.registry.security.user.oidc.discovery.url'           "${NIFI_REGISTRY_SECURITY_OIDC_DISCOVERY_URL}"
-prop_replace 'nifi.registry.security.user.oidc.client.id'               "${NIFI_REGISTRY_SECURITY_OIDC_CLIENT_ID}"
-prop_replace 'nifi.registry.security.user.oidc.client.secret'           "${NIFI_REGISTRY_SECURITY_OIDC_CLIENT_SECRET}"
-
